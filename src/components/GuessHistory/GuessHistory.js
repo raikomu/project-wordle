6a1afb019947c5e87b4699d304c8fb770d736203
Raiko Murulauk <raiko4238@gmail.com>
2025-07-16 21:08:01 +0300

    Display guess history grid

--- src/components/GuessHistory/GuessHistory.js
+++ src/components/GuessHistory/GuessHistory.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Guess from '../Guess/Guess'
+
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import { range } from '../../utils'
 
 function GuessHistory({ guessHistory }) {
   return (
     <div className="guess-results">
       {guessHistory.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+        <Guess key={index} guess={guess} />
+      ))}
+      {range(guessHistory.length, NUM_OF_GUESSES_ALLOWED).map((index) => (
+        <Guess key={index} guess={''} />
       ))}
     </div>
   )
